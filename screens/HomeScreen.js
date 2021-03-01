9865f4db977d3edc802b959fcb7f544b6cc1034d
Robert <robertwt7@gmail.com>
2021-03-01 23:48:15 +1100

    Updated calendar props

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -24,40 +24,24 @@ export default function AccountsScreen() {
           console.log("selected day", day);
         }}
         // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-        monthFormat="yyyy MM"
+        monthFormat="MM yyyy"
         // Handler which gets executed when visible month changes in calendar. Default = undefined
         onMonthChange={(month) => {
           console.log("month changed", month);
         }}
-        // Hide month navigation arrows. Default = false
-        hideArrows
-        // Replace default arrows with custom ones (direction can be 'left' or 'right')
-        renderArrow={(direction) => <Arrow />}
-        // Do not show days of other months in month page. Default = false
-        hideExtraDays
         // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
         // day from another month that is visible in calendar page. Default = false
         disableMonthChange
         // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
         firstDay={1}
-        // Hide day names. Default = false
-        hideDayNames
         // Show week numbers to the left. Default = false
         showWeekNumbers
         // Handler which gets executed when press arrow icon left. It receive a callback can go back month
         onPressArrowLeft={(subtractMonth) => subtractMonth()}
         // Handler which gets executed when press arrow icon right. It receive a callback can go next month
         onPressArrowRight={(addMonth) => addMonth()}
-        // Disable left arrow. Default = false
-        disableArrowLeft
-        // Disable right arrow. Default = false
-        disableArrowRight
         // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
         disableAllTouchEventsForDisabledDays
-        // Replace default month and year title with custom one. the function receive a date as parameter.
-        renderHeader={(date) => {
-          /* Return JSX */
-        }}
         // Enable the option to swipe between months. Default = false
         enableSwipeMonths
       />
