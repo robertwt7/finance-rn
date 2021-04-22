ab62ce5d3df25a3cfe1562f4ff9117279d81d052
Robert <robertwt7@gmail.com>
2021-04-22 22:59:17 +1000

    Fixed cyclic dependencies issue in TransactionConst for validation

--- src/constants/forms/TransactionConst.js
+++ src/constants/forms/TransactionConst.js
@@ -1,25 +1,28 @@
 import * as yup from "yup";
 import dayjs from "dayjs";
 
-export const validationTransaction = yup.object().shape({
-  categoryId: yup.number().when("name", {
-    is: (name) => !name,
-    then: yup
-      .number()
-      .typeError("Must be filled if name is not filled")
-      .required("Required"),
-    otherwise: yup.number().nullable(),
-  }),
-  name: yup.string().when("categoryId", {
-    is: (categoryId) => !categoryId,
-    then: yup
-      .string()
-      .typeError("Must be filled if category is not choosed")
-      .required("Required"),
-    otherwise: yup.string().nullable(),
-  }),
-  amount: yup.number().typeError("Must be a number").required("Required"),
-});
+export const validationTransaction = yup.object().shape(
+  {
+    categoryId: yup.number().when("name", {
+      is: (name) => !name,
+      then: yup
+        .number()
+        .typeError("Must be filled if name is not filled")
+        .required("Required"),
+      otherwise: yup.number().nullable(),
+    }),
+    name: yup.string().when("categoryId", {
+      is: (categoryId) => !categoryId,
+      then: yup
+        .string()
+        .typeError("Must be filled if category is not choosed")
+        .required("Required"),
+      otherwise: yup.string().nullable(),
+    }),
+    amount: yup.number().typeError("Must be a number").required("Required"),
+  },
+  [["categoryId", "name"]]
+);
 
 const values = {
   name: "",
